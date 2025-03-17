c50fad036374a2f360977b150651727caffc7c9d
Nayem5522 <sahidhasannayembd@gmail.com>
2025-03-18 00:14:27 +0600

    Update info.py

--- info.py
+++ info.py
@@ -142,7 +142,7 @@ REACTIONS = ["🤝", "😇", "🤗", "😍", "👍", "🎅", "😐", "🥰", "�
 
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
