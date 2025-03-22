d708520be7ed756c65870e540b9494b3e91a5731
Nayem5522 <sahidhasannayembd@gmail.com>
2025-03-22 21:27:34 +0600

    Update info.py

--- info.py
+++ info.py
@@ -26,7 +26,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://envs.sh/Nx0.jpg")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002184630716'))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5926160191').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5274915656 5926160191').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002342980086').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
