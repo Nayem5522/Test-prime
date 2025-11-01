01a77dab15fa4a3b859e06a0ef6df69c7b115c56
Nayem5522 <sahidhasannayembd@gmail.com>
2025-11-01 17:16:13 +0600

    Update info.py

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', 'pri_Nay1') # Public Ch
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/cinelolmovie')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mhmoviebackup')
 MAINCHANL_LNK = environ.get('MAINCHANL_LNK', 'https://t.me/First_Uploading_Content')
-PSUP_LNK = environ.get('PSUP_LNK', 'https://t.me/MHBDDiscussionGruop')
+PSUP_LNK = environ.get('PSUP_LNK', 'https://t.me/first_uploading_contents')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Prime_Movie_Watch_Dawnload/75')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'peoples_disscussion_group') # Support Chat Link Without https:// or @
 
@@ -175,5 +175,6 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
