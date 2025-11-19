dfa754173182af778ba941e811deaa1db5d44ab0
Nayem5522 <sahidhasannayembd@gmail.com>
2025-11-19 10:59:15 +0600

    Update info.py

--- info.py
+++ info.py
@@ -81,7 +81,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mhmoviebackup')
 MAINCHANL_LNK = environ.get('MAINCHANL_LNK', 'https://t.me/First_Uploading_Content')
 PSUP_LNK = environ.get('PSUP_LNK', 'https://t.me/first_uploading_contents')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/Prime_Movie_Watch_Dawnload/75')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'peoples_disscussion_group') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'First_Uploading_Content') # Support Chat Link Without https:// or @
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
@@ -175,6 +175,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
 
 
 
