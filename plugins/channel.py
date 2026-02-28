ea8cd4bc75be5785c7fa706d420ba98df329562b
Nayem5522 <sahidhasannayembd@gmail.com>
2026-02-28 12:52:20 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -62,7 +62,7 @@ async def media_old(bot, message):
 # 2. NEW HANDLER (UPCHANNELS)
 # ====================================================================
 
-@Client.on_message(filters.chat(UPCHANNELS) & media_filter)
+@Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media_new(bot, message):
     for file_type in ("document", "video", "audio"):
         media = getattr(message, file_type, None)
@@ -417,5 +417,6 @@ async def fetch_tmdb_data(query, year=None):
 def generate_unique_id(movie_name):
     return hashlib.md5(movie_name.encode('utf-8')).hexdigest()[:5]
     
+
 
 
